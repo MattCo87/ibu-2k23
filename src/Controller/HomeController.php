438a83491ec943c5eb46be6290dab12e6858f42b
MattCo87 <77285498+MattCo87@users.noreply.github.com>
2022-05-14 19:41:28 +0200

    first init v23

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Run;
+use App\Repository\RunRepository;
+use phpDocumentor\Reflection\Types\Null_;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="app_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+        ]);
+    }
+
+}
