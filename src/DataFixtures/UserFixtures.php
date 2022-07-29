834599870d4d08688292aaafe2ef3e57b352c08b
MattCo87 <77285498+MattCo87@users.noreply.github.com>
2022-07-29 02:07:25 +0200

    Init Biath

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserFixtures extends Fixture implements OrderedFixtureInterface
+{
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $tabuser = [];
+        $tabuser = [
+            ["test@gmail.com", "ROLE_ADMIN", "testettestera"],
+        ];
+        
+        foreach ($tabuser as list($email, $role, $pwd)) {
+            $user = new User();
+            $password = $this->hasher->hashPassword($user, $pwd);
+            $user
+                ->setEmail($email)
+                ->setRoles([$role])
+                ->setPassword($password);
+            $manager->persist($user);
+        }
+        unset($tabuser, $email, $role, $pwd);
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 10;
+    }
+}
